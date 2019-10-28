1045df9f5831c0e004ad73554c6ed66a7295a835
bestokes <bestokes@expedia.com>
2019-10-28 08:33:37 +0000

    Fixes for weather.sh

--- weather.sh
+++ weather.sh
@@ -2,31 +2,29 @@
 
 export graphiteHost="127.0.0.1"
 export graphitePort="2003"
-export scriptDir=/root/hive
+export scriptDir=/home/ben/weather
 
 
 function GetOutsideTemperature() {
+        if [ -f "${scriptDir}/json.out" ]; then rm -f ${scriptDir}/json.out; fi 
         export gotWeatherData=False
         openweathermapKey=$(cat ${scriptDir}/openweathermapKey)
-        outsideTemperature=$(curl -s "https://api.openweathermap.org/data/2.5/weather?lat=50.9243&lon=-0.1454&units=metric&appid=${openweathermapKey}" | grep -o -E '"temp":([0-9]+\.[0-9]+)' | awk -F: '{ print $2 }')
+        curl -s "https://api.openweathermap.org/data/2.5/weather?lat=50.9243&lon=-0.1454&units=metric&appid=${openweathermapKey}" | json_pp > ${scriptDir}/json.out 
+        outsideTemperature=$(cat ${scriptDir}/json.out | jq '.main.temp')
+        windSpeed=$(cat ${scriptDir}/json.out | jq '.wind.speed')
+
         if [ "$?" != "0" ]; then
                 echo "ERROR API call failed to openweathermap.org"
         else
                 export gotWeatherData=True
         fi
-        # I'm parsing this with grep because jq doesn't
-        # like the start and end of the json
 
-        if [ "${gotWeatherData}" = "True" ]; then
-                actualTemperature=$(cat ${scriptDir}/nodes.out | jq '.nodes[4].attributes.temperature.reportedValue')
-        else
-                actualTemperature=Null
-        fi
 }
 
 function PostToGraphite() {
         echo "INFO Sending weather data to graphite"
         echo "weather.outsideTemperature ${outsideTemperature} $(date +%s)" | nc -w 2 ${graphiteHost} ${graphitePort}
+        echo "weather.windSpeed ${windSpeed} $(date +%s)" | nc -w 2 ${graphiteHost} ${graphitePort}
 }
 
 
